a60e9c4e56891a28b6cba33ad64ed9fa9639bef4
lvyou-huang <119716788+lvyou-huang@users.noreply.github.com>
2022-12-31 14:45:29 +0800

    并发(d)

---
+++ bingfa(d).go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	var a int = 1
+	var ch = make(chan int, 1)
+	go work(&a, ch)
+	go work(&a, ch)
+	go work(&a, ch)
+	go work(&a, ch)
+	go work(&a, ch)
+	<-ch
+
+}
+
+func work(a *int, ch chan int) {
+	var flag bool = true
+	for ; *a < 10000000; *a++ {
+		for i := 2; i < *a/2; i++ {
+			if *a%i == 0 {
+				flag = false
+			}
+		}
+		if flag {
+			fmt.Println(*a)
+		} else {
+			flag = true
+		}
+	}
+	ch <- 1
+
+}
